c53986447ddfe65d5e0b9be068fae7a85a3119a1
luke_lin <luke_lin@kingdee.com>
2014-04-04 10:32:08 +0800

    add stack example

--- karma.config.js
+++ karma.config.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'polyn-test.js'
+      'stack-test.js'
     ],
 
     // list of files / patterns to exclude
---
+++ stack-test.js
@@ -0,0 +1,75 @@
+function Stack(){
+  this.top = null;
+  this.size = 0;
+}
+Stack.prototype = {
+  push: function(data){
+    var node = {
+      data: data,
+      next: null
+    };
+
+    node.next = this.top;
+    this.top = node;
+    this.size++;
+  },
+  peek: function(){
+    return this.top === null ?
+      null :
+      this.top.data;
+  },
+  pop: function(){
+    if(this.top === null) return null;
+
+    var out = this.top;
+    this.top = this.top.next;
+
+    if(this.size > 0) this.size--;
+
+    return out.data;
+  },
+  displayAll: function(){
+    if(this.top === null) return null;
+
+    var arr = [];
+    var current = this.top;
+
+    for(var i = 0, len = this.size; i < len; i++){
+      arr[i] = current.data;
+      current = current.next;
+    }
+
+    return arr;
+  }
+};
+
+describe('stack tests', function(){
+  var stack = new Stack();
+
+
+
+  it('should push into stack', function(){
+    stack.push(1);
+    expect(stack.peek()).toBe(1);
+    stack.push('asd');
+    expect(stack.peek()).toBe('asd');
+    expect(stack.size).toBe(2);
+  });
+
+  it('should pop from stack', function(){
+    stack.pop();
+    expect(stack.peek()).toBe(1);
+    expect(stack.size).toBe(1);
+    stack.push({a: 1});
+    expect(stack.peek()).toEqual({a: 1});
+    expect(stack.size).toBe(2);
+  });
+
+  it('should be an empty stack', function(){
+    stack.pop();
+    expect(stack.peek()).toBe(1);
+    stack.pop();
+    expect(stack.peek()).toBe(null);
+    expect(stack.size).toBe(0);
+  });
+});
