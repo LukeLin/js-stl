b21c2985ad30d51d58d7c0f81778a0cdc59a182c
luke_lin <luke_lin@kingdee.com>
2014-09-09 16:08:38 +0800

    modified

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -253,6 +253,52 @@ AdjacencyMatrixGraph.prototype = {
         }
 
         return true;
+    },
+
+    /**
+     * 删除顶点
+     * @param {String} vex 要删除的顶点
+     */
+    deleteVex: function(vex){
+        var n = this.vexnum - 1;
+        var m = this.locateVex(vex);
+
+        if(m < 0) return false;
+
+        // 将待删除顶点交换到最后一个顶点
+        var temp = this.vexs[m];
+        this.vexs[m] = this.vexs[n];
+        this.vexs[n] = temp;
+
+        // 将边的关系随之交换
+        for(var i = 0; i <= n; i++){
+            this.arcs[i][m] = this.arcs[i][n];
+            this.arcs[m][i] = this.arcs[n][i];
+        }
+
+        this.arcs[m][m].adj = 0;
+        this.vexs.length = --this.vexnum;
+        return true;
+    },
+
+    /**
+     * 删除边(v, w)
+     * @param {String} v
+     * @param {String} w
+     * @returns {boolean}
+     */
+    deleteArc: function(v, w){
+        var i = this.locateVex(v);
+        var j = this.locateVex(w);
+
+        if(i < 0 || j < 0) return false;
+
+        if(this.arcs[i][j].adj) {
+            this.arcs[i][j].adj = 0;
+            this.arcnum--;
+        }
+
+        return true;
     }
 };
 
@@ -727,4 +773,73 @@ OLGraph.prototype = {
 var g = new OLGraph();
 g.createDG();
 console.log(g);
+
+
+/*
+邻接多重表
+
+邻接多重表(Adjacency Multilist)是无向图的另一种链式存储结构。
+
+邻接表是无向图的一种有效的存储结构，在无向图的邻接表中，一条边(v,w)的两个表结点分别初选在以v和w为头结点的链表中，很容易求得顶点和边的信息，但在涉及到边的操作会带来不便。
+
+邻接多重表的结构和十字链表类似，每条边用一个结点表示；邻接多重表中的顶点结点结构与邻接表中的完全相同，而表结点包括六个域。
+
+◆  Data域：存储和顶点相关的信息；
+◆ 指针域firstedge：指向依附于该顶点的第一条边所对应的表结点；
+◆ 标志域mark：用以标识该条边是否被访问过；
+◆ ivex和jvex域：分别保存该边所依附的两个顶点在图中的位置；
+◆ info域：保存该边的相关信息；
+◆ 指针域ilink：指向下一条依附于顶点ivex的边；
+◆ 指针域jlink：指向下一条依附于顶点jvex的边；
+
+邻接多重表与邻接表的区别：
+后者的同一条边用两个表结点表示，而前者只用一个表结点表示；除标志域外，邻接多重表与邻接表表达的信息是相同的，因此，操作的实现也基本相似。
+
+ */
+
+var UNVISITED = 0;
+var VISITED = 1;
+
+/**
+ * 边的结点表示
+ * @param {Number} mark 访问标记 0 -- 未访问  1 == 已访问
+ * @param {Number} ivex 该边依附的两个顶点的位置
+ * @param {Number} jvex 该边依附的两个顶点的位置
+ * @param {EBox} ilink 分别指向依附这两个顶点的下一条边
+ * @param {EBox} jlink 分别指向依附这两个顶点的下一条边
+ * @param {*} info 该边信息
+ * @constructor
+ */
+function EBox(mark, ivex, jvex, ilink, jlink, info){
+    this.mark = mark || UNVISITED;
+    this.ivex = ivex || 0;
+    this.jvex = jvex || 0;
+    this.ilink = ilink || null;
+    this.jlink = jlink || null;
+    this.info = info || null;
+}
+
+/**
+ * 顶点的结点表示
+ * @param {*} data
+ * @param {EBox} firstEdge 指向第一条依附该顶点的边
+ * @constructor
+ */
+function AMLVexBox(data, firstEdge){
+    this.data = data || null;
+    this.firstEdge = firstEdge || null;
+}
+
+/**
+ *
+ * @param {Array | AMLVexBox} adjMulist
+ * @param {Number} vexnum
+ * @param {Number} edgenum
+ * @constructor
+ */
+function AMLGraph(adjMulist, vexnum, edgenum){
+    this.adjMulist = adjMulist || [];
+    this.vexnum = vexnum || 0;
+    this.edgenum = edgenum || 0;
+}
 
