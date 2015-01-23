e3d5da243c39d6a1e3d27c79b57960a715335220
luke_lin <luke_lin@kingdee.com>
2015-01-23 09:49:21 +0800

    add TrieTree implemention and merge DST into a file

--- Search/DigitalSearchTree.js
+++ Search/DigitalSearchTree.js
@@ -221,3 +221,140 @@ test.remove('CHEN');
 test.remove('LI');
 test.remove('LIU');
 test.remove('ZHAO');
+
+
+
+
+/*
+多重链表表示
+
+若以树的多重链表表示键树，则树的每个结点中应含有d个指针域，此时的键树又称Trie树。
+（Trie是从检索retrieve中取中间四个字符的，读音同try）。
+若从键树中某个结点到叶子结点的路径上每个结点都只有一个孩子，则可将该路径上所有结点压缩成一个“叶子结点”，且在该叶子结点中存储关键字及指向记录的指针等信息。
+在Trie树中有两种结点：
+分支结点：含有d个指针域和一个指示该结点中非空指针域的个数的整数域。在分支结点中不设数据域，每个分支结点所表示的字符均有其父结点中指向该结点的指针所在位置决定。
+叶子结点：含有关键字域和指向记录的指针域。
+
+
+在Trie树上进行查找
+
+从根结点出发，沿和给定值相应的指针逐层向下，直至叶子结点，若叶子结点中的关键字和给定值相等，则查找成功，若分支结点中和给定值相应的指针为空，或叶子结点中的关键字和给定值不相等，则查找不成功。
+ */
+
+
+function TrieTree(kind){
+    this.kind = kind || BRANCH;
+    this.leaf = {
+        key: null,
+        info: null
+    };
+    this.branch = {
+        nodes: new Array(27),
+        num: 0
+    };
+}
+TrieTree.prototype = {
+    constructor: TrieTree,
+
+    search: function(key) {
+        for (var p = this, i = 0;
+             p && p.kind === BRANCH && i < key.length;
+             p = p.branch.nodes[order(key[i])], ++i);
+
+        if(p && p.kind === LEAF && p.leaf.key === key) return p.leaf.info;
+        else return null;
+    },
+
+    insert: function(key, value){
+        // 建叶子结点
+        var q = new TrieTree();
+        q.kind = LEAF;
+        q.leaf.key = key;
+        q.leaf.info = value;
+
+        // 自上而下查找
+        var last;
+        for(var p = this, i = 0;
+            p && i < key.length && p.branch.nodes[order(key[i])];
+            p = p.branch.nodes[order(key[i])], ++i) last = p;
+
+        // 如果最后落到分支结点（无同义词）
+        // 直接连上叶子
+        if(p.kind === BRANCH) {
+            p.branch.nodes[order(key[i])] = q;
+            ++p.branch.num;
+        }
+        // 如果最后落到叶子结点（有同义词）
+        else {
+            // 建立新的分支结点
+            var r = new TrieTree();
+            // 用新的分支结点取代老叶子结点和上一层的联系
+            last.branch.nodes[order(key[i - 1])] = r;
+            r.kind = BRANCH;
+            r.branch.num = 2;
+            r.branch.nodes[order(key[i])] = q;
+            // 新分支结点与新老两个叶子结点相连 todo bug exists
+            r.branch.nodes[order(p.leaf.key[i])] = p;
+        }
+    },
+
+    remove: function(key){
+        var last;
+        // 查找待删除元素
+        for(var p = this, i = 0;
+            p && p.kind === BRANCH && i < key.length;
+            p = p.branch.nodes[order(key[i])], ++i) last = p;
+
+        if(p && p.kind === LEAF && p.leaf.key === key) {
+            last.branch.nodes[order(key[i - 1])] = null;
+            return true;
+        } else return false;
+    }
+
+};
+
+// 求字符在字母表中的序号
+function order(c){
+    return c.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+}
+
+
+
+var test = new TrieTree();
+
+test.insert('CHA');
+test.insert('CHANG');
+test.insert('CAI');
+test.insert('CHEN');
+test.insert('CAO');
+test.insert('CHAO');
+test.insert('LONG');
+test.insert('LI');
+test.insert('LAN');
+test.insert('LIU');
+test.insert('WANG');
+test.insert('WEN');
+test.insert('WU');
+test.insert('YANG');
+test.insert('YUN');
+test.insert('ZHAO');
+
+test.search('YUN');
+test.search('ZHAO');
+
+test.remove('LAN');
+test.remove('LIU');
+test.remove('WANG');
+test.remove('WEN');
+test.remove('WU');
+test.remove('YANG');
+test.remove('YUN');
+test.remove('ZHAO');
+test.remove('CHA');
+test.remove('CHANG');
+test.remove('CAI');
+test.remove('CHEN');
+test.remove('CAO');
+test.remove('CHAO');
+test.remove('LONG');
+test.remove('LI');
