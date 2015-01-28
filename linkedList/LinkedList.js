9032f7937909bd48aa3acea1d1950c464038a9a1
luke_lin <luke_lin@kingdee.com>
2015-01-28 10:44:47 +0800

    调整LinkedList的api

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -279,91 +279,91 @@
             q.next = p;
             s.next = q;
             this.head = s;
-        },
-
-        // 求元素递增排列的线性表A和B的元素的交集并存入C
-        intersect: function (bList) {
-            var cList = new List();
-
-            var p = this.head;
-            var q = bList.head;
-
-            while (p && q) {
-                if (p.data < q.data) p = p.next;
-                else if (q.data > q.data) q = q.next;
-                else {
-                    cList.add(q.data);
-                    p = p.next;
-                    q = q.next;
-                }
-            }
-
-            return cList;
-        },
-
-        // 求元素递增排列的线性表A和B的元素的交集并存入回a
-        intersect_true: function (bList) {
-            var p = this.head;
-            var q = bList.head;
-            var pc = this.head;
-
-            while (p && q) {
-                if (p.data < q.data) p = p.next;
-                else if (p.data > q.data) q = q.next;
-                else {
-                    pc.data = p.data;
-                    p = p.next;
-                    q = q.next;
-
-                    if (!p || !q) {
-                        pc.next = null;
-                        this.tail = pc;
-                    } else pc = pc.next;
-                }
-            }
-
-            pc.next = null;
-            this.tail = pc;
-        },
-
-        // a，b，c的元素均是非递减排列
-        // 求a链表中非b链表和c链表的交集的元素。
-        intersect_delete: function (b, c) {
-            var p = b.head;
-            var q = c.head;
-            var r = this.head;
-
-            while (p && q && r) {
-                if (p.data < q.data) p = p.next;
-                else if (p.data > q.data) q = q.next;
-                else {
-                    // 确定待删除元素
-                    var elem = p.data;
-
-                    if(r.data === elem && r === this.head) {
-                        this.head = this.head.next;
-                    } else {
-                        // 确定最后一个小于elem的元素指针
-                        while (r.next && r.next.data < elem) r = r.next;
-
-                        if (r.next.data === elem) {
-                            var s = r.next;
-
-                            // 确定第一个大于elem的元素指针
-                            while (s && s.data === elem) s = s.next;
-                            // 删除r和s之间的元素
-                            r.next = s;
-                        }
-                    }
-
-                    while (p && p.data === elem) p = p.next;
-                    while (q && q.data === elem) q = q.next;
-                }
-            }
-
-            this.tail = r;
         }
     };
+
+    // 求元素递增排列的线性表A和B的元素的交集并存入C
+    function intersect (list, bList) {
+        var cList = new List();
+
+        var p = list.head;
+        var q = bList.head;
+
+        while (p && q) {
+            if (p.data < q.data) p = p.next;
+            else if (q.data > q.data) q = q.next;
+            else {
+                cList.add(q.data);
+                p = p.next;
+                q = q.next;
+            }
+        }
+
+        return cList;
+    }
+
+    // 求元素递增排列的线性表A和B的元素的交集并存入回a
+    function intersect_true(list, bList) {
+        var p = list.head;
+        var q = bList.head;
+        var pc = list.head;
+
+        while (p && q) {
+            if (p.data < q.data) p = p.next;
+            else if (p.data > q.data) q = q.next;
+            else {
+                pc.data = p.data;
+                p = p.next;
+                q = q.next;
+
+                if (!p || !q) {
+                    pc.next = null;
+                    list.tail = pc;
+                } else pc = pc.next;
+            }
+        }
+
+        pc.next = null;
+        list.tail = pc;
+    }
+
+    // a，b，c的元素均是非递减排列
+    // 求a链表中非b链表和c链表的交集的元素。
+    function intersect_delete (list, b, c) {
+        var p = b.head;
+        var q = c.head;
+        var r = list.head;
+
+        while (p && q && r) {
+            if (p.data < q.data) p = p.next;
+            else if (p.data > q.data) q = q.next;
+            else {
+                // 确定待删除元素
+                var elem = p.data;
+
+                if(r.data === elem && r === list.head) {
+                    list.head = list.head.next;
+                } else {
+                    // 确定最后一个小于elem的元素指针
+                    while (r.next && r.next.data < elem) r = r.next;
+
+                    if (r.next.data === elem) {
+                        var s = r.next;
+
+                        // 确定第一个大于elem的元素指针
+                        while (s && s.data === elem) s = s.next;
+                        // 删除r和s之间的元素
+                        r.next = s;
+                    }
+                }
+
+                while (p && p.data === elem) p = p.next;
+                while (q && q.data === elem) q = q.next;
+            }
+        }
+
+        list.tail = r;
+    }
 
     var list = new List();
     list.add('b');
@@ -429,9 +429,9 @@
     b.orderInsert(9);
     b.orderInsert(13);
     b.orderInsert(17);
-    console.log(a.intersect(b));
+    console.log(intersect(a, b));
 
-    console.log(a.intersect_true(b));
+    console.log(intersect_true(a, b));
 
     a = new List();
     a.orderInsert(1);
@@ -449,7 +449,7 @@
     test.orderInsert(6);
     test.orderInsert(9);
 
-    test.intersect_delete(a, b);
+    intersect_delete(test, a, b);
     console.log(test);
 
 }());
