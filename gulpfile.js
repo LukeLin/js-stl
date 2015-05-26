71d9bcd489dd7b8cca9fbc68566214b1c24bbd5c
LukeLin <773000785@qq.com>
2015-05-26 19:35:05 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,27 @@ var del = require('del');
 var livereload = require('gulp-livereload');
 
 gulp.task('scripts', function () {
-    return gulp.src('2/scripts/*.es6')
+    return gulp.src('*/**/*.es6')
         .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(gulp.dest('2/scripts'))
-        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        //.pipe(concat('all.js'))
         .pipe(browserify({
             insertGlobals : false,
             debug : !gulp.env.production
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('2/dist'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function(cb){
-    del(['2/dist/*.js'], cb);
+    del(['dist/*/**/*.js'], cb);
 });
 
 gulp.task('watch', function(){
-    gulp.watch(['2/scripts/*.es6'], ['scripts']);
+    gulp.watch(['*/**/*.es6'], ['scripts']);
     livereload.listen();
-    gulp.watch(['2/**']).on('change', livereload.changed);
+    gulp.watch(['*/**/*.es6']).on('change', livereload.changed);
 });
 
 gulp.task('default', ['clean', 'scripts', 'watch']);
