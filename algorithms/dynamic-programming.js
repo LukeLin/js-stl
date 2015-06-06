c98482ac634a24feed0c16d234d80690358753af
luke <773000785@qq.com>
2015-06-06 11:31:38 +0800

    add 最长单调递增子序列2

--- algorithms/dynamic-programming.js
+++ algorithms/dynamic-programming.js
@@ -24,32 +24,32 @@
  子问题重叠性质。子问题重叠性质是指在用递归算法自顶向下对问题进行求解时，每次产生的子问题并不总是新问题，有些子问题会被重复计算多次。动态规划算法正是利用了这种子问题的重叠性质，对每一个子问题只计算一次，然后将其计算结果保存在一个表格中，当再次需要计算已经计算过的子问题时，只是在表格中简单地查看一下结果，从而获得较高的效率。
  */
 
-(function(){
+(function () {
     // 从n个物件里取出m个的组合数（0 <= m <= n）
 
     // 分治法 时间复杂度为O(C(n, m))
     /*
-    递归式：
-        C(n, m) = C(n - 1, m) + C(n - 1, m - 1)
+     递归式：
+     C(n, m) = C(n - 1, m) + C(n - 1, m - 1)
      */
-    function divide_comb(n, m){
-        if(m === 0 || n === m) return 1;
+    function divide_comb(n, m) {
+        if (m === 0 || n === m) return 1;
         else return divide_comb(n - 1, m) + divide_comb(n - 1, m - 1);
     }
 
     console.log(divide_comb(11, 5));    // 462
 
     // 动态规划优化1  时间复杂度O(n*m) 空间复杂度O(n*m)
-    function dynamic_comb(n, m){
+    function dynamic_comb(n, m) {
         var c = [];
-        for(var i = 0; i <= n; ++i){
+        for (var i = 0; i <= n; ++i) {
             c[i] = [];
             c[i][0] = 1;
         }
-        for(var j = 1; j <= m; ++j)
+        for (var j = 1; j <= m; ++j)
             c[0][j] = 0;
-        for(i = 1; i <= n; ++i){
-            for(j = 1; j <= m; ++j){
+        for (i = 1; i <= n; ++i) {
+            for (j = 1; j <= m; ++j) {
                 c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
             }
         }
@@ -61,14 +61,14 @@
 
 
     // 动态规划优化1  时间复杂度O(n*m) 空间复杂度O(m)
-    function dynamic_comb2(n, m){
-        if(m === 0) return 1;
+    function dynamic_comb2(n, m) {
+        if (m === 0) return 1;
 
         var c = [0];
-        if(m > 0) {
-            for(var i = 1; i <= n; ++i) c[i] = 0;
-            for(i = 1; i <= n; ++i){
-                for(var j = m + 1; j > 1; --j){
+        if (m > 0) {
+            for (var i = 1; i <= n; ++i) c[i] = 0;
+            for (i = 1; i <= n; ++i) {
+                for (var j = m + 1; j > 1; --j) {
                     c[j] += c[j - 1];
                 }
                 ++c[i];
@@ -82,52 +82,85 @@
 
 })();
 
-(function(){
+(function () {
     // 最长单调递增子序列
 
     // 时间复杂度O(n^2)算法
-    function LISdyna(arr){
+    function LISdyna(arr) {
         var n = arr.length;
         var b = [];
         b[0] = 0;
-        for(var i = 1; i < n; ++i){
+        for (var i = 1; i < n; ++i) {
             var k = 0;
-            for(var j = 0; j < i; ++j){
-                if(arr[j] <= arr[i] && k < b[j])
+            for (var j = 0; j < i; ++j) {
+                if (arr[j] <= arr[i] && k < b[j])
                     k = b[j];
                 else k = 0;
             }
             b[i] = k + 1;
         }
 
-        for(i = 0, j = 0; i < n; ++i){
-            if(b[i] > j) j = b[i];
+        for (i = 0, j = 0; i < n; ++i) {
+            if (b[i] > j) j = b[i];
         }
 
         return j;
     }
 
     console.log('最长单调递增子序列1:');
-    console.log(LISdyna([3,2,4,1,2,7,8,9,10]));
+    console.log(LISdyna([3, 2, 4, 1, 2, 7, 8, 9, 10]));
+
+
+    // O(nlogn)时间复杂度
+    // 一个长度为i的候选子序列的最后一个元素至少与一个长度为i-1的候选子序列的最后一个元素一样大。
+    // 通过只想输入序列中的元素的指针来维持候选子序列
+    // http://www.cppblog.com/mysileng/archive/2012/11/30/195841.html
+    function LISdyna2(arr) {
+        var b = [];
+        b[1] = arr[0];
+        var k = 1;
+
+        for (var i = 1; i < arr.length; ++i) {
+            if (arr[i] >= b[k]) b[++k] = arr[i];
+            else b[binary(i, k)] = arr[i];
+        }
+        return k;
+
+        // 二分搜索：在b[1..k]中找到下标j,使得b[j - 1] <= a[i] < b[j]
+        // 时间复杂度为O(logk)
+        function binary(i, k) {
+            if (arr[i] < b[1]) return 1;
+            var j = k;
+            for (var h = 1; h !== j - 1;) {
+                if (b[k = Math.floor((h + j) / 2)] <= arr[i]) h = k;
+                else j = k;
+            }
+
+            return j;
+        }
+    }
+
+    console.log('最长单调递增子序列2:');
+    console.log(LISdyna2([3, 2, 4, 1, 2, 7, 8, 9, 10]));
 })();
 
 // unoptimized version
 // 两矩阵相乘
-function matrixMultiply(a, b){
+function matrixMultiply(a, b) {
     var c = [];
     var ra = a[0].length;
     var ca = a.length;
     var rb = b[0].length;
     var cb = b.length;
 
-    if(ca !== rb) throw new Error('matrix can\'t be multiplied!');
+    if (ca !== rb) throw new Error('matrix can\'t be multiplied!');
 
-    for(var i = 0; i < ra; ++i){
-        if(!c[i]) c[i] = [];
-        for(var j = 0; j < cb; ++j){
+    for (var i = 0; i < ra; ++i) {
+        if (!c[i]) c[i] = [];
+        for (var j = 0; j < cb; ++j) {
             var sum = a[i][0] * b[0][j];
 
-            for(var k = 1; k < ca; ++k)
+            for (var k = 1; k < ca; ++k)
                 sum += a[i][k] * b[k][j];
 
             c[i][j] = sum;
@@ -153,8 +186,8 @@ var m2 = [
 console.log(matrixMultiply(m2, m1));
 
 /*
-[ [ 0, 0, 0, 0 ],
-  [ 0, 0, -2, 0 ],
-  [ 0, 3, 0, 5 ],
-  [ 0, -6, -4, -10 ] ]
+ [ [ 0, 0, 0, 0 ],
+ [ 0, 0, -2, 0 ],
+ [ 0, 3, 0, 5 ],
+ [ 0, -6, -4, -10 ] ]
  */
