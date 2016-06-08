acd593815bf009d6c3b71b0474188cee8348dd81
LukeLin <773000785@qq.com>
2016-06-08 18:11:25 +0800

    update

--- src/Queue/CycleQueue.js
+++ src/Queue/CycleQueue.js
@@ -3,54 +3,54 @@
  */
 
 // 循环队列
-function CycleQueue() {
-    this.base = {};
-    this.front = this.rear = 0;
-    this.MAXQSIZE = 100;
-}
-exports.CycleQueue = CycleQueue;
-CycleQueue.prototype = {
-    enQueue: function (data) {
+export default class CycleQueue {
+    constructor(){
+        this.base = {};
+        this.front = this.rear = 0;
+        this.MAXQSIZE = 100;
+    }
+
+    enQueue(data) {
         if ((this.rear + 1) % this.MAXQSIZE === 0) throw new Error('cycleQueue is already full!');
 
         this.base[this.rear] = data;
         this.rear = (this.rear + 1) % this.MAXQSIZE;
-    },
-    deQueue: function () {
+    }
+    deQueue() {
         if (this.front === this.rear) throw new Error('cycleQueue is already empty');
 
         var elem = this.base[this.front];
         this.front = (this.front + 1) % this.MAXQSIZE;
 
         return elem;
-    },
-    clear: function () {
+    }
+    clear() {
         this.base = {};
         this.front = this.rear = 0;
-    },
+    }
     get size() {
         return (this.rear - this.front + this.MAXQSIZE) % this.MAXQSIZE;
-    },
-    peekAt: function (index) {
-        index = (index || 0 + this.MAXQSIZE) % this.MAXQSIZE;
+    }
+    peekAt(index = 0) {
+        index = (index + this.MAXQSIZE) % this.MAXQSIZE;
 
         return this.base[index + this.front] || null;
-    },
-    getHead: function () {
+    }
+    getHead() {
         var elem = this.base[this.front];
         return elem ? elem : null;
-    },
-    queueTraverse: function (iterator) {
+    }
+    queueTraverse(iterator) {
         for (var i = this.front, len = this.rear = this.front; i < len; i++) {
             if (iterator(this.base[i], i)) break;
         }
-    },
-    toString: function () {
+    }
+    toString() {
         var base = [].slice.call(this.base);
 
         return base.slice(this.front, this.rear - this.front);
     }
-};
+}
 
 var queue = new CycleQueue();
 queue.enQueue(1);
