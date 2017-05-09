18d043e8f54659cde48d3c7aa06ecadc065d9638
LukeLin <773000785@qq.com>
2017-05-09 20:03:13 +0800

    add todo

---
+++ src/List/SkipList.js
@@ -0,0 +1,9 @@
+/**
+ * 跳跃链表（SkipList）
+ * 
+ * @related
+ * http://lightthewoods.me/2017/04/25/index-data-structure-besides-books/
+ * http://lightthewoods.me/2017/04/25/index-data-structure-besides-books/
+ */
+
+
