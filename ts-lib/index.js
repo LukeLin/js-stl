b61beb08f7297e6429b53a13e7b4c9f6d49ae946
LukeLin <773000785@qq.com>
2018-03-16 11:21:37 +0800

    update

--- ts-lib/index.js
+++ ts-lib/index.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const DoubleLinkedList_1 = require("./List/DoubleLinkedList");
 const DoubleLinkedList_2 = require("./List/DoubleLinkedList");
-const LRUCache_ts_1 = require("./List/LRUCache.ts");
+const LRUCache_1 = require("./List/LRUCache");
+const StaticLinkedList_1 = require("./List/StaticLinkedList");
 exports.default = {
     List: {
         DoubleLinkedList: DoubleLinkedList_1.default,
         LinkedList: DoubleLinkedList_2.default,
-        LRUCache: LRUCache_ts_1.default
+        LRUCache: LRUCache_1.default,
+        StaticLinkedList: StaticLinkedList_1.default
     }
 };
 //# sourceMappingURL=index.js.map
--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,11 +1,13 @@
 import DoubleLinkedList from './List/DoubleLinkedList';
 import LinkedList from './List/DoubleLinkedList';
 import LRUCache from './List/LRUCache';
+import StaticLinkedList from './List/StaticLinkedList';
 
 export default {
     List: {
         DoubleLinkedList,
         LinkedList,
-        LRUCache
+        LRUCache,
+        StaticLinkedList
     }
 };
