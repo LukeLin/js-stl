514052fc171030f396183cd771bf3d2cf7e99e1b
LukeLin <773000785@qq.com>
2019-01-29 11:24:43 +0800

    update

---
+++ ts-src/Queue/Queue.ts
@@ -0,0 +1,108 @@
+/**
+ * 队列Queue
+ *
+ * 队列是一种先进先出（first in first out, FIFO）的线性表。它只允许在表的一端进行插入，而在另一端删除元素。
+ * 允许插入的一端叫队尾（rear），允许删除的一端叫队头（front）。
+ */
+
+interface QueueNode {
+    data: any,
+    next: QueueNode | null
+}
+
+// 链队列
+export default class Queue {
+    rear: QueueNode;
+    front: QueueNode;
+    size: number;
+
+    constructor() {
+        this.rear = this.front = null;
+        this.size = 0;
+    }
+
+    isEmpty () {
+        return this.rear === null;
+    }
+
+    clear () {
+        this.rear = this.front = null;
+        this.size = 0;
+    }
+
+    getHead () {
+        return this.front ? this.front.data : null;
+    }
+
+    enQueue (elem: any) {
+        if (this.front === null) {
+            this.rear = this.front = { data: elem, next: null };
+        } else {
+            let p: QueueNode = { data: elem, next: null };
+            this.rear.next = p;
+            this.rear = p;
+        }
+        this.size++;
+    }
+
+    deQueue () {
+        if (this.front) {
+            let elem = this.front.data;
+            this.front = this.front.next;
+            if (this.front === null) {
+                this.rear = null;
+            }
+            this.size--;
+            return elem;
+        } else {
+            return null;
+        }
+    }
+
+    forEach (iterator: Function) {
+        if (typeof iterator !== 'function')
+            throw new Error('iterator should be function');
+
+        let current = this.front;
+        while (current) {
+            if (iterator(current.data)) break;
+            current = current.next;
+        }
+    }
+
+    *[Symbol.iterator] () {
+        let current = this.front;
+        while (current) {
+            yield current.data;
+            current = current.next;
+        }
+    }
+
+    peekAt (index = 0) {
+        if (index < this.size) {
+            let current = this.front;
+            for (let i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current.data;
+        }
+
+        return -1;
+    }
+
+    toString () {
+        if (this.front === null) {
+            return null;
+        }
+
+        let arr = [];
+        let current = this.front;
+
+        for (let i = 0, len = this.size; i < len; i++) {
+            arr[i] = current.data;
+            current = current.next;
+        }
+
+        return arr;
+    }
+}
---
+++ ts-src/Stack/index.ts
@@ -0,0 +1,82 @@
+/**
+ * 栈
+ *
+ * 是限定仅在表尾进行插入或删除操作的线性表。表尾为栈顶（top），表头为栈底（bottom），不含元素的空表为空栈。
+ * 栈又称为后进先出（last in first out）的线性表。
+ */
+
+/**
+ * 顺序栈
+ * 栈的顺序存储结构是利用一组地址连续的存储单元依次存放至栈底到栈顶的元素，同时附设指针top指示栈顶元素在顺序栈中的位置。
+ *
+ */
+
+interface StackNode {
+    data: any;
+    next: StackNode | null
+}
+
+// 栈的链式表示
+export default class Stack {
+    top: StackNode;
+    length: number;
+
+    constructor() {
+        this.top = null;
+        this.length = 0;
+    }
+
+    get size () {
+        return this.length;
+    }
+
+    isEmpty () {
+        return this.length === 0;
+    }
+
+    push (data: any) {
+        let node: StackNode = {
+            data,
+            next: null
+        };
+
+        node.next = this.top;
+        this.top = node;
+        this.length++;
+    }
+
+    peek (): any {
+        return this.top === null ?
+            null :
+            this.top.data;
+    }
+
+    pop () {
+        if (this.top === null) return null;
+
+        let out = this.top;
+        this.top = this.top.next;
+
+        if (this.length > 0) this.length--;
+
+
+        return out.data;
+    }
+    clear () {
+        this.top = null;
+        this.length = 0;
+    }
+    toString (): string {
+        if (this.top === null) return null;
+
+        let arr = [];
+        let current = this.top;
+
+        for (let i = 0, len = this.size; i < len; i++) {
+            arr[i] = current.data;
+            current = current.next;
+        }
+
+        return arr.join(',');
+    }
+}
