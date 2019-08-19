ece5c6d97aa4d5d80be811e747beb54a9ac2811a
LukeLin <773000785@qq.com>
2019-08-19 16:58:55 +0800

    add

--- src/algorithms/examples.js
+++ src/algorithms/examples.js
@@ -83,3 +83,29 @@
 
     convert('LEETCODEISHIRING', 3);
 })();
+
+(() => {
+    // https://leetcode-cn.com/problems/regular-expression-matching/
+    var isMatch = function(s, p) {
+        if(!p) return !s.length;
+        
+        if(p.length === 1) {
+            return (s.length === 1 && (s[0] === p[0] || p[0] === '.'));
+        }
+        
+        if(p[1] !== '*') {
+            if(!s) return false;
+            
+            return (s[0] === p[0] || p[0] === '.') && isMatch(s.substr(1), p.substr(1));
+        }
+        
+        while(s && (s[0] === p[0] || p[0] === '.')) {
+            if(isMatch(s, p.substr(2))) return true;
+            s = s.substr(1);
+        }
+        
+        return isMatch(s, p.substr(2));
+    };
+
+    console.log(isMatch('aab', 'c*a*b'));
+})();
